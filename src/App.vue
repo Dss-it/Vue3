785e91f83aea1493c1adbb48fadc0035fd4f1283
songsong.Du <dusongsong@xiaohongshu.net>
2024-04-30 17:11:29 +0800

    feat: project  init

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import home from '@/pages/home/index.vue'
+</script>
+
+<template>
+  <div>
+    <a href="https://vitejs.dev" target="_blank">
+      <img src="/vite.svg" class="logo" alt="Vite logo" />
+    </a>
+    <a href="https://vuejs.org/" target="_blank">
+      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+    </a>
+  </div>
+  <home></home>
+</template>
+
+<style scoped>
+
+</style>
