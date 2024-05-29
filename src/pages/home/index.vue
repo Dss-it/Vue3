a1521a69d79dcb5928a3499fda96be94a84edde9
songsong.Du <dusongsong@xiaohongshu.net>
2024-05-29 16:24:57 +0800

    feat: add  log

--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -6,8 +6,10 @@
 
 <script setup lang='ts'>
 import { ref } from "vue" 
-
-
+// .env.development
+ let flag = import.meta.env.VITE_BASE_API
+ console.log(flag);
+ 
 </script>
 
 <style lang="scss" scoped>
